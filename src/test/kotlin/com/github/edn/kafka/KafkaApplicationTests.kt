330bb7c9e5325a3b6aff864a789109581ae3f1e4
Edinei Piovesan da Silva <edinei.live@gmail.com>
2020-08-14 15:58:29 -0300

    Remove SpringBoot context test

--- src/test/kotlin/com/github/edn/kafka/KafkaApplicationTests.kt
+++ src/test/kotlin/com/github/edn/kafka/KafkaApplicationTests.kt
@@ -1,13 +1,12 @@
 package com.github.edn.kafka
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class KafkaApplicationTests {
 
     @Test
-    fun contextLoads() {
+    fun `just an empty test`() {
+
     }
 
 }
